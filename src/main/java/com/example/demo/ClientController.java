76b6b5f18ced7e924802bd328664db3d28785741
praneeth00007 <2300031137@kluniversity.in>
2025-10-14 20:04:49 +0530

    initial commit

---
+++ src/main/java/com/example/demo/ClientController.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ClientController 
+{
+	@GetMapping("/")
+	public String home() {
+		return "CI/CD cloud devops";
+	}
+	
+	@GetMapping("/demo")
+	public String demo() {
+		return "Jenkins Demo";
+	}
+	
+	@GetMapping("/test")
+	public String test() {
+		return "testing for a purpose";
+	}
+	
+	
+
+}
